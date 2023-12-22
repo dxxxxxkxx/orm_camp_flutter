014e786817f435d638debccf1e17fef8b8ec8ec7
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-22 13:00:49 +0900

    전자 갤러리

---
+++ lib/2023_12_21/digital_gallery/grid_page.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class GridPage extends StatefulWidget {
+  const GridPage({super.key});
+
+  @override
+  State<GridPage> createState() => _GridPageState();
+}
+
+class _GridPageState extends State<GridPage> {
+  final ImagePicker _picker;
+  final PageController _controller;
+  List<XFile>? _imageFileList;
+  int _currentPage;
+
+  _GridPageState()
+      : _picker = ImagePicker(),
+        _controller = PageController(),
+        _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getImageFileList();
+  }
+
+  Future<void> _getImageFileList() async {
+    _imageFileList = await _picker.pickMultiImage();
+
+    if (_imageFileList != null) {
+      Timer.periodic(const Duration(seconds: 5), (timer) {
+        if (++_currentPage > _imageFileList!.length - 1) {
+          _currentPage = 0;
+        }
+
+        _controller.animateToPage(_currentPage,
+            duration: const Duration(milliseconds: 350), curve: Curves.easeIn);
+      });
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('전자 갤러리')),
+      body: _imageFileList != null
+          ? PageView(
+              controller: _controller,
+              children: _imageFileList!.map((final XFile imageFile) {
+                return FutureBuilder<Uint8List>(
+                  future: imageFile.readAsBytes(),
+                  builder: (final BuildContext context,
+                          final AsyncSnapshot<Uint8List> snapshot) =>
+                      ((snapshot.data != null) &&
+                              (snapshot.connectionState !=
+                                  ConnectionState.waiting))
+                          ? Image.memory(snapshot.data!, width: double.infinity)
+                          : const Center(child: CircularProgressIndicator()),
+                );
+              }).toList(),
+            )
+          : const Center(child: Text('No image file')),
+    );
+  }
+}
---
+++ lib/2023_12_21/digital_gallery/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'grid_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      title: '전자 갤러리',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const GridPage(),
+    );
+  }
+}
