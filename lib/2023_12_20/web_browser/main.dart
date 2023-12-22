6a44eeb2ad555a8f9509ac77f2eaa978443b0c8e
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-22 10:06:39 +0900

    웹 브라우저 뒤로가기 미완성

---
+++ lib/2023_12_20/web_browser/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      title: '웹 브라우저',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
---
+++ lib/2023_12_20/web_browser/main_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late final WebViewController _controller;
+  final String _url;
+
+  _MainPageState() : _url = 'https://www.google.com/';
+
+  @override
+  void initState() {
+    super.initState();
+
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''Page resource error:
+            code: ${error.errorCode}
+            description: ${error.description}
+            errorType: ${error.errorType}
+            isForMainFrame: ${error.isForMainFrame}''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse(_url));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('웹 브라우저'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (final String url) =>
+                _controller.loadRequest(Uri.parse(url)),
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: 'https://www.google.com/',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem(
+                value: 'https://www.naver.com/',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem(
+                value: 'https://www.daum.net/',
+                child: Text('다음'),
+              )
+            ],
+          )
+        ],
+      ),
+      body: PopScope(
+          onPopInvoked: (final bool didPop) async {
+            if (await _controller.canGoBack()) {
+              await _controller.goBack();
+            }
+          },
+          child: WebViewWidget(controller: _controller)),
+    );
+  }
+}
