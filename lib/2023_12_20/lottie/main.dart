eb6229b64c77d7e0996ffb35f6af03da421aa868
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-26 03:31:32 +0900

    Lottie

---
+++ lib/2023_12_20/lottie/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'lottie_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      title: 'Lottie',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LottieScreen(),
+    );
+  }
+}
